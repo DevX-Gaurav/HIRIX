a1f590d942c7fb9bc426669f89691904f4e403ab
Gaurav_Jha <jhagaurav82003@gmail.com>
2025-09-19 11:08:10 +0530

    change made in the cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    methode: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
