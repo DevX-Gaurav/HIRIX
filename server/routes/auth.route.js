694da03b2af467c981819476777eaf9919819e81
Gaurav_Jha <jhagaurav82003@gmail.com>
2025-09-19 11:33:09 +0530

    few changes made to the upload controllers

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -10,16 +10,27 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/get", protect, getMe);
 
+// router.post("/upload-image", upload.single("image"), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).json({
+//       message: "No file uploaded",
+//     });
+//   }
+//   const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+//     req.file.filename
+//   }`;
+//   res.status(200).json({ imageUrl });
+// });
+
 router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({
       message: "No file uploaded",
     });
   }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-  }`;
-  res.status(200).json({ imageUrl }); 
+
+  // Cloudinary gives URL in req.file.path
+  res.status(200).json({ imageUrl: req.file.path });
 });
 
 module.exports = router;
